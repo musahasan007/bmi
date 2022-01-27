e015b68a195051a064248bd8644fc0ede9f83af9
unknown <you@example.com>
2022-01-27 15:30:59 +0600

    ggfsdr

---
+++ lib/constants.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+const double kBottomContainerHeight = 80.0;
+const Color kBottomContainerColor = Color(0xFFEB1555);
+const Color kActiveCardColor = Color(0xFF1D1E33);
+const Color kInactiveCardColor = Color(0xFF111328);
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xFF8D8E98),
+);
+
+const kNumberTextStyle = TextStyle(fontSize: 45.0, fontWeight: FontWeight.w900);
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 40.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultTextStyle = TextStyle(
+  color: Color(0xFF24D876),
+  fontSize: 18.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBMITextStyle = TextStyle(
+  fontSize: 80.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultBodyTextStyle = TextStyle(
+  fontSize: 18.0,
+);
